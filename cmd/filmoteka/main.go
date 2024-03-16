67af18a6038339276b3247773f6cfa67c3e1991f
PoorMercymain <pinegin17@bk.ru>
2024-03-16 23:36:03 +0300

    http method for test handler added, typo fixed

--- cmd/filmoteka/main.go
+++ cmd/filmoteka/main.go
@@ -27,11 +27,15 @@ func main() {
 		return
 	}
 
+	logger.Logger().Infoln("Migrations applied successfully")
+
 	pool, err := repository.GetPgxPool(dsn)
 	if err != nil {
 		logger.Logger().Fatalln(zap.Error(err))
 		return
 	}
+
+	logger.Logger().Infoln("Postgres connection pool created")
 
 	r := repository.New(repository.NewPostgres(pool))
 	s := service.New(r)
@@ -39,7 +43,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/ping", h.Ping)
+	mux.HandleFunc("GET /ping", h.Ping)
 
 	server := &http.Server{
 		Addr:     "localhost:8080",
@@ -48,6 +52,7 @@ func main() {
 	}
 
 	go func() {
+		logger.Logger().Infoln("Server started, listening on port", 8080)
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			logger.Logger().Fatalln("ListenAndServe failed", zap.Error(err))
 		}
@@ -65,8 +70,8 @@ func main() {
 	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
-		logger.Logger().Fatalln("Server were forced to shutdown:", zap.Error(err))
+		logger.Logger().Fatalln("Server was forced to shutdown:", zap.Error(err))
 	}
 
-	logger.Logger().Infoln("Server were shut down")
+	logger.Logger().Infoln("Server was shut down")
 }
